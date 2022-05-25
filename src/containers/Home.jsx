bf51c4186dd718928a194e8abb909dc93380a18b
julianest <julianest12@gmail.com>
2022-05-25 18:43:59 -0500

    [Fix] Se arregla pantalla inicial

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-import MapP from "../components/MapP";
 import SubNav from "../components/SubNav";
-import WeatherWeekly from "../components/WeatherWeekly";
+
 
 
 const Home = () => {
   return (
     <>
       <SubNav />
-      <WeatherWeekly />
-      <MapP />
     </>
   );
 };
